f642e9ac806bb5be0631ae20f8b7452818dbc149
cuongaz25 <huydo1388@gmail.com>
2025-10-30 17:52:49 +0700

    Delete src/main/java/com/craftvn/craftanti directory

--- src/main/java/com/craftvn/craftanti/util/Color.java
+++
@@ -1,9 +0,0 @@
-package com.craftvn.craftanti.util;
-
-import org.bukkit.ChatColor;
-
-public class Color {
-    public static String colorize(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s == null ? "" : s);
-    }
-}
